f4508c50030db73a167c0b5e75bf0b1cc1912770
Bing Ren <bingtimren@gmail.com>
2021-03-24 20:35:53 +1100

    added package greeting

--- libs/word/tests/package/package.test.ts
+++
@@ -1,8 +0,0 @@
-// import from package with absolute path and test
-import {WORD} from '@bingsjs/word'
-
-describe("package test",()=>{
-    it("should be able to import from package with absolute path", ()=>{
-        expect(typeof WORD).toEqual("string");
-    })
-})
