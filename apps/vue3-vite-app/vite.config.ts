3cc5234c566fd67fadda3a9b2a09e364500070a0
Bing Ren <bingtimren@gmail.com>
2021-04-06 09:32:30 +1000

    (1) added built for ES6 module for @bingjs/greeting & @bingsjs/word (2) removed work arounds in Vite

--- apps/vue3-vite-app/vite.config.ts
+++ apps/vue3-vite-app/vite.config.ts
@@ -6,12 +6,17 @@ import commonjs from '@rollup/plugin-commonjs';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  optimizeDeps: {
-    include: ['@bingsjs/greeting']
-  },
-  build: {
-    rollupOptions: {
-      plugins:[ resolve(), commonjs()]
-    }
-  }
+  // 
+  // **** Below are work-arounds to solve problems when (1) packages are symbol linked and (2) built for CommonJS ****
+  // **** See https://github.com/vitejs/vite/issues/2697 ****
+  // **** Since @bingsjs/greeting is now built for ES2015 as well, the work-arounds below are not needed ****
+  //
+  // optimizeDeps: {
+  //   include: ['@bingsjs/greeting']
+  // },
+  // build: {
+  //   rollupOptions: {
+  //     plugins:[ resolve(), commonjs()]
+  //   }
+  // }
 })
