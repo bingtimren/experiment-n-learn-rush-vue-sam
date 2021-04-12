958379792c2cd76aa7b93b07141a4dc356a5fe1c
Bing Ren <bingtimren@gmail.com>
2021-04-12 16:08:06 +1000

    test git pre-commit hook prettier

--- libs/greeting/src/commons.ts
+++ libs/greeting/src/commons.ts
@@ -2,6 +2,7 @@
  * @packageDocumentation
  * Artifacts shared by both front-end and backend
  */
+// try to 'uglify' and test git commit hook to prettier
 import { WORD } from '@bingsjs/word';
 /**
  * Just an example function
